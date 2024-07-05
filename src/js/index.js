0e49bd8370be5140ab94b8da0fd248f6f21622c9
HasanC14 <chowdhuryh999@gmail.com>
2024-07-06 04:40:05 +0600

    footer table and other sections added

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".loader-wrapper").style.display = "none";
 });
 const HandleNavbar = () => {
-  $(".nav_content").toggleClass("hidden");
-  $(".nav_bg").toggleClass("bg-white");
+  $(".nav_content").toggleClass(
+    "hidden transition-all duration-500 ease-in-out"
+  );
+  $(".nav_bg").toggleClass("bg-white transition-all duration-700 ease-in-out");
   $(".nav_btn").toggleClass("active");
 };
 
@@ -12,4 +14,19 @@ document.addEventListener("scroll", function () {
   const scrollPosition = window.scrollY;
   const parallaxElement = document.querySelector(".background");
   parallaxElement.style.transform = `translateY(${scrollPosition * 0.5}px)`;
+});
+
+document.addEventListener("scroll", function () {
+  const navbar = document.querySelector("nav");
+  if (window.scrollY > window.innerHeight) {
+    navbar.classList.add("bg-white");
+  } else {
+    navbar.classList.remove("bg-white");
+  }
+});
+
+$("#table").DataTable({
+  searching: true,
+  ordering: true,
+  paging: true,
 });
